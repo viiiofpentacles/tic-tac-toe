14e8e59027d8e1826bbcd38dabce51d6cce7f33c
bea <chamoischamois@protonmail.com>
2022-02-25 16:34:05 +0800

    Stopped players from selecting marked squares

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const gameboard = (() => {
         for (let i = 0; i < 9; i++) {
         const square = document.createElement("div");
         
-        square.addEventListener("click", () => {
+        square.addEventListener("click", addMark)
+        
+        function addMark() {
 
             const numberOfX = gameBoardSquares.filter(square => square === "x");
             const numberOfO = gameBoardSquares.filter(square => square === "o");
@@ -16,22 +18,26 @@ const gameboard = (() => {
                 square.textContent = "☓";
                 squareIndex = gameBoardSquares.indexOf(square);
                 gameBoardSquares[squareIndex] = "x";
+                square.removeEventListener("click", addMark, false)
             }else{
                 square.textContent = "○";
                 squareIndex = gameBoardSquares.indexOf(square);
                 gameBoardSquares[squareIndex] = "o";
+                square.removeEventListener("click", addMark, false)
             }
-        })
+        }
 
         gameBoardContainer.appendChild(square);
         gameBoardSquares.push(square);
         }
-        
+                  
     })();
 
     return {gameBoardSquares};
 
 })()
+
+
 
 // Winning conditions used to check for winner 
 const gamePlay = () => {
