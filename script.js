5a39b08fc33f91ccb53cdb3f9109da31aa79f7a4
bea <chamoischamois@protonmail.com>
2022-02-25 16:15:27 +0800

    Added script to alternate gameboard with circles and crosses

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const Gameboard = (() => {
+const gameboard = (() => {
     const gameBoardContainer = document.getElementById("gameboard-container");
+
+    const gameBoardSquares = [];
+
     const createGameBoardSquares = (() => {
-        const gameBoardSquares = [];
         for (let i = 0; i < 9; i++) {
         const square = document.createElement("div");
+        
+        square.addEventListener("click", () => {
+
+            const numberOfX = gameBoardSquares.filter(square => square === "x");
+            const numberOfO = gameBoardSquares.filter(square => square === "o");
+           
+            if(numberOfX.length === 0 || numberOfX.length === numberOfO.length || numberOfO.length > numberOfX.length){
+                square.textContent = "☓";
+                squareIndex = gameBoardSquares.indexOf(square);
+                gameBoardSquares[squareIndex] = "x";
+            }else{
+                square.textContent = "○";
+                squareIndex = gameBoardSquares.indexOf(square);
+                gameBoardSquares[squareIndex] = "o";
+            }
+        })
+
         gameBoardContainer.appendChild(square);
         gameBoardSquares.push(square);
         }
-        return gameBoardSquares;
+        
     })();
-    
+
+    return {gameBoardSquares};
 
 })()
+
+// Winning conditions used to check for winner 
+const gamePlay = () => {
+
+}
